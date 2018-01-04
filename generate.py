9f47539c7b3f50c12367e5a061225a6606396c1a
VladimirTolmachev(Home OSX) <tolm_vl@hotmail.com>
2018-01-05 00:36:47 +0700

    renamed server -> server_python

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import os
 
 os.system('python ./mlc/src/main.py -i ./configs -o ./client/mg -l py -f xml -side client -data ./configs/data -data_out ./client/assets')
-os.system('python ./mlc/src/main.py -i ./configs -o ./server/mg -l py -f xml -side server -data ./configs/data -data_out ./server')
+os.system('python ./mlc/src/main.py -i ./configs -o ./server_python/mg -l py -f xml -side server -data ./configs/data -data_out ./server_python')
 os.system('python ./mlc/src/main.py -i ./configs -o ./server_php/mg -l php -f xml -side server -data ./configs/data -data_out ./server_php')
 os.system('python ./mlc/src/main.py -i ./configs -o ./server_cpp/mg -l cpp -f xml -side server -data ./configs/data -data_out ./server_cpp')
